53728970ce41f5c9bd244ea5d019376dc3dddc06
OLAMIDE <OLAMIDE@192.168.2.10>
2016-09-01 19:27:58 -0400

    Initial commit of project files

---
+++ spwebhomework/src/main/java/com/ola/SpwebhomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.ola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpwebhomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpwebhomeworkApplication.class, args);
+	}
+}
