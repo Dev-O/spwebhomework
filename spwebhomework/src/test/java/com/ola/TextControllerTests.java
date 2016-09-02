e9afca403d16a7d17acd708e7c6e0b4944cddba5
OLAMIDE <OLAMIDE@192.168.2.10>
2016-09-02 08:21:19 -0400

    new endpoint methods test cases

--- spwebhomework/src/test/java/com/ola/TextControllerTests.java
+++ spwebhomework/src/test/java/com/ola/TextControllerTests.java
@@ -4,46 +4,118 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.*;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.web.WebAppConfiguration;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.mockito.Mockito.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import org.springframework.http.MediaType;
 
 @WebAppConfiguration
-public class TextControllerTests extends SpwebhomeworkApplication{
+public class TextControllerTests extends SpwebhomeworkApplicationTests{
 	
-     private MockMvc mockMvc;
-
+   private MockMvc mockMvc;
+	  //  TextController textControllerMock;  
+	
 	 @Mock
-     private  TextingService textServiceMock;
+    private  TextingService textServiceMock;
 	 
 	 @InjectMocks
 	 private TextController textControllerMock;
-	 	 
+	 
+	 //TextController textControllerMock = org.mockito.Mockito.mock(TextController.class);
+	 
 	 @Before
 	 public void setup() {
 		 MockitoAnnotations.initMocks(this);
 		 mockMvc = standaloneSetup(textControllerMock)
-		                 .build();      
+		                 .build();
+	        
 	    }
 	 
 	 @Test
 	    public void createAndsendUserTextTest() throws Exception {
-	    	String txt = "I am here";
-	    	TextModel textModel = new TextModel();
-	    	textModel.setText(txt);
-	    	when(textServiceMock.sendText(txt)).thenReturn(textModel);
-	    	mockMvc.perform(post("/texts").param("text" , txt))
+	    	long time = System.currentTimeMillis();
+		    Timestamp timestamp = new Timestamp(time);
+	    	String texton = "I am here";
+	    	String userNamere = "pala";
+	    	TextModel myText = new TextModel (texton, userNamere,timestamp);		
+	    	when(textServiceMock.saveText(myText)).thenReturn(myText);
+	    	String texton1 = "I am here";
+	    	String userNamere1 = "pala";
+	    	MultiValueMap<String,String> paramVal= new   LinkedMultiValueMap<String, String>();
+	        paramVal.add("text", texton1);
+	        paramVal.add("userName",userNamere1);
+	    	mockMvc.perform(post("/texts").params(paramVal))
 	         .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
 		        .andExpect(status().isCreated())
-		        .andExpect(jsonPath("$.text").value("I am here"));    	
+		        .andExpect(jsonPath("$.text").value("I am here"))
+	            .andExpect(jsonPath("$.userName").value("pala"));    	
 	    }
+
 	 
+	    @Test
+	    public void getAlltextShouldReturnAlltextsTest() throws Exception {
+	    	long time = System.currentTimeMillis();
+		    Timestamp timeStamp = new Timestamp(time);
+		    TextModel  txt1 = new TextModel ("Bada","hello Bada", timeStamp);
+	    	long time2 = System.currentTimeMillis();
+		    Timestamp timeStamp2 = new Timestamp(time2);
+		    TextModel  txt2 = new TextModel ("Aaron","hello Aaron", timeStamp2);
+	    	long time3 = System.currentTimeMillis();
+		    Timestamp timeStamp3 = new Timestamp(time3);
+		    TextModel  txt3 = new TextModel ("Collins","hello Collins", timeStamp3);
+	    	long time4 = System.currentTimeMillis();
+		    Timestamp timeStamp4 = new Timestamp(time4);
+		    TextModel  txt4 = new TextModel ("Andrew","hello Andrew",timeStamp4 );
+	    	List<TextModel > listOfTexts = new ArrayList<>();
+	    	listOfTexts.add(txt1);
+	    	listOfTexts.add(txt2);
+	    	listOfTexts.add(txt3);
+	    	listOfTexts.add(txt4);
+	        when(textServiceMock.getALLTexts()).thenReturn(listOfTexts);
+
+	        mockMvc.perform(get("/texts") .contentType(MediaType.APPLICATION_JSON_UTF8))
+	            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+	            .andExpect(status().isOk())
+	            .andExpect(jsonPath("$[0].userName").value("Bada"))
+	            .andExpect(jsonPath("$[1].userName").value("Aaron"))
+	            .andExpect(jsonPath("$[2].userName").value("Collins"))
+	            .andExpect(jsonPath("$[0].text").value("hello Bada"))
+	            .andExpect(jsonPath("$[1].text").value("hello Aaron"))
+	            .andExpect(jsonPath("$[2].text").value("hello Collins"));
+	    }    
+	   
+	       
+	    @Test
+	    public void getSpecificUserTextsTest1() throws Exception {
+	    	long time = System.currentTimeMillis();
+		    Timestamp timeStamp = new Timestamp(time);
+		    long time2 = System.currentTimeMillis();
+		    Timestamp timeStamp2 = new Timestamp(time2);
+	    	String userName = "Awa";
+	    	TextModel txt1 = new TextModel("Awa","hello Awa", timeStamp);
+	    	TextModel txt2 = new TextModel("Awa","hi Awa", timeStamp2);
+	    	List<TextModel> listOfTexts = new ArrayList<>();
+	    	listOfTexts.add(txt1);
+	    	listOfTexts.add(txt2);
+	    	 when(textServiceMock.getUserTexts(userName)).thenReturn(listOfTexts);
+	        mockMvc.perform(get("/texts/" +userName))
+	           .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+		        .andExpect(status().isOk())
+		        .andExpect(jsonPath("$", hasSize(2)))
+	            .andExpect(jsonPath("$.[0].userName").value("Awa"))
+	            .andExpect(jsonPath("$.[0].text").value("hello Awa"));
+	            	    }
 }
---
+++ spwebhomework/src/test/java/com/ola/TextRepositoryTests.java
@@ -0,0 +1,152 @@
+package com.ola;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@WebAppConfiguration
+public class TextRepositoryTests extends SpwebhomeworkApplicationTests {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	public TextRepository textRepository;
+	
+	Long fOid, sOid, tOid;
+	
+	Long[] arrayOfIds= new Long[]{fOid, sOid, tOid};
+	
+	@Before
+    public void setUp() {
+		textRepository.deleteAll();
+        // fill database with some values
+    	long time = System.currentTimeMillis();
+	    Timestamp timestamp = new Timestamp(time);
+	    textRepository.save(new TextModel("shannon", "I see you shannon",timestamp));
+		long time2 = System.currentTimeMillis();
+	    Timestamp timestamp2 = new Timestamp(time2);
+	    textRepository.save(new TextModel("shannon", "I am here now",timestamp2));
+		long time3 = System.currentTimeMillis();
+	    Timestamp timestamp3 = new Timestamp(time3);
+	    textRepository.save(new TextModel("Arnold", "Hello Arnold",timestamp3));
+		 List<TextModel> listOfTexts = new ArrayList<TextModel>();
+		 listOfTexts  = textRepository.findAllTexts();
+	     
+    Iterable<TextModel> x = textRepository.findAll();
+    for(TextModel y : x){
+    	
+    	if (y.getUserName().equalsIgnoreCase("shannon") && y.getText().equalsIgnoreCase("I see you shannon")){
+    		
+    	    fOid = y.getOid();}
+    	
+    	 if (y.getUserName().equalsIgnoreCase("shannon") && y.getText().equalsIgnoreCase("I am here now")){
+    		
+    	     sOid = y.getOid();
+    	}
+    		
+    	if (y.getUserName().equalsIgnoreCase("Arnold")){
+    		
+    		tOid = y.getOid();
+    	}
+    		 
+    	}
+    	
+    }
+    
+    @Transactional
+    @Rollback(true)
+    @Test    
+    public  void createbNewTextInDbShouldReturnTextCreated(){
+     	    Long tempOid = null;
+     	try{	
+     		logger.info("Test started at" + " " + ":" + LocalDateTime.now());
+     		long time = System.currentTimeMillis();
+     	    Timestamp timestamp = new Timestamp(time);
+     	    textRepository.save(new TextModel("Lee", "well lets see",timestamp));
+     	    for (TextModel x : textRepository.findAll()){
+     	    	
+     	    	if (Arrays.binarySearch(arrayOfIds,x.getOid() , null)<0){
+     	    		tempOid = x.getOid();
+     	    		
+     	    	}
+     	    	
+     	    }
+     		assertEquals(textRepository.findByOid(tempOid).getText(), "well lets see");
+     	}	
+     	catch(Exception ex){
+     		logger.debug("Test is aborted due to Exception" + " : "+ ex.getMessage());	
+     	}
+     	
+     }
+     
+    @Transactional
+    @Rollback(true)
+    @Test    
+   public  void findTextByIDShouldReturnTextStoredByTheID(){
+     	
+     	try{	
+     		logger.info("Test started at" + " " + ":" + LocalDateTime.now());
+     		assertEquals(textRepository.findByOid(fOid).getText(), "I see you shannon");
+     		assertEquals(textRepository.findByOid(sOid).getText(), "I am here now");
+     	}	
+     	catch(Exception ex){
+     		logger.debug("Test is aborted due to Exception" + " : "+ ex.getMessage());	
+     	}
+     	
+     }
+
+    @Transactional
+    @Rollback(true)
+    @Test
+  public   void FindTextByUserNameShouldReturnListOfUserTexts(){
+     	try{
+     		logger.info("Test started at" + " " + ":" + LocalDateTime.now() );
+     		List<TextModel> listOfUsersTexts = textRepository.findByUserName("shannon");
+     		assertTrue(listOfUsersTexts.size()>0);
+     		assertEquals(listOfUsersTexts.get(0).getText(),"I see you shannon");
+     		assertEquals(listOfUsersTexts.get(1).getText(),"I am here now");
+     		assertEquals(listOfUsersTexts.get(2).getText(),"Hello Arnold");
+     	}
+     	catch(Exception ex){
+     		logger.debug("This is a debug message" + " "+ ex.getMessage());	
+     	}	
+     }
+
+    @Transactional
+    @Rollback(true)
+    @Test
+ public    void getAllTextsInDBShouldReturnListOfAllTextIndb(){
+     	
+     	try{
+     		logger.info("Test started at" + " " + ":" + LocalDateTime.now());
+     		assertTrue(textRepository.findAllTexts().size()>2);
+     		assertEquals(textRepository.findAllTexts().get(0).getText(),"I see you shannon");
+     		assertEquals(textRepository.findAllTexts().get(1).getText(),"I am here now");
+     		assertEquals(textRepository.findAllTexts().get(2).getText(),"Hello Arnold");
+     		
+     	}
+     	catch(Exception ex){
+     		logger.debug("This is a debug message" + " "+ ex.getMessage());	
+     	}
+     		
+     	
+     }
+     
+   
+
+}
