381762d8076009e6252de1a8582e36fe6daf3ee0
OLAMIDE <OLAMIDE@192.168.2.10>
2016-09-02 08:19:46 -0400

    Controller and service methods to retrieve user tests

--- spwebhomework/src/main/java/com/ola/TextController.java
+++ spwebhomework/src/main/java/com/ola/TextController.java
@@ -1,7 +1,9 @@
 package com.ola;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.nio.charset.Charset;
+import java.sql.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Component
 @RestController
@@ -24,14 +27,62 @@ public class TextController {
 	@RequestMapping(method = RequestMethod.POST, 
             produces = "application/json; charset=UTF-8")
     public  ResponseEntity<TextModel> getNdReturnUserText(
-    		@RequestParam(value = "text", required = true) String userSentText)
+    		 @RequestParam(value = "text", required = true) String text, 
+    	       @RequestParam(value = "userName", required = true) String userName)
 {  
-		HttpHeaders responseHeaders = new  HttpHeaders();   
-		responseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-		TextModel textt = textService.sendText(userSentText);
-	        return new ResponseEntity<TextModel>(textt ,responseHeaders, HttpStatus.CREATED);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);	    
+		long time = System.currentTimeMillis();
+	    Timestamp timestamp = new Timestamp(time);
+		TextModel usertext = new TextModel(userName, text, timestamp);
+		TextModel txt = textService.saveText(usertext);
+	        return new ResponseEntity<TextModel>(txt ,responseHeaders, HttpStatus.CREATED);
 	    }
 
+	
+	
+	
+	// Endpoint to retrieve all user texts
+		@RequestMapping(value = "{userName}", method = RequestMethod.GET, 
+	            produces = "application/json; charset=UTF-8")
+	    public ResponseEntity<List<TextModel>> getAllUserTexts(
+	    		@org.springframework.web.bind.annotation.PathVariable("userName")  String userName) {
+			final HttpHeaders httpHeaders = new HttpHeaders();
+		    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+			List<TextModel> listOfUsersTexts = new java.util.ArrayList<>();
+			if(userName !=null){
+				 listOfUsersTexts= textService.getUserTexts(userName);
+				
+				 if (!listOfUsersTexts.equals(null)){
+				 return new ResponseEntity<List<TextModel>>(listOfUsersTexts,httpHeaders , HttpStatus.OK);
+			}
+			
+				 else
+				 {
+				return new ResponseEntity<List<TextModel>>(HttpStatus.INTERNAL_SERVER_ERROR);
+				 }
+			}
+			else
+				// if request parameter is missing retunr back request to the user
+			return new ResponseEntity<List<TextModel>>(HttpStatus.BAD_REQUEST);
+	    }
+		
+		
+	
+		@RequestMapping(method = RequestMethod.GET, 
+	            produces = "application/json; charset=UTF-8")
+	    public  ResponseEntity< List<TextModel>> getAllText() {
+			HttpHeaders httpHeaders = new HttpHeaders();
+			httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+			List<TextModel> listOfTexts = new ArrayList<TextModel>();
+			listOfTexts = 	textService.getALLTexts();
+		 return new ResponseEntity<List<TextModel>>(listOfTexts,  httpHeaders, HttpStatus.OK);
+				  
+			}	
+		
 	}
+
+
+
 
 
--- spwebhomework/src/main/java/com/ola/TextingService.java
+++ spwebhomework/src/main/java/com/ola/TextingService.java
@@ -1,16 +1,94 @@
 package com.ola;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 
 @Service
-
 public class TextingService {
-	TextModel sendText(String userSentText){
-		TextModel userText = new TextModel();
-		userText.setText(userSentText);
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	public TextRepository textRepository;
+	
+
+	// get all the texts in db
+	List<TextModel> getALLTexts(){
 		
-		return userText;
+		try{
+		List<TextModel> listOfAllTextsInDb = textRepository.findAllTexts();
+		return listOfAllTextsInDb;	
+		}
+		
+		catch(Exception ex){
+			
+			logger.debug( " This is an error " + " " + ex.getMessage());		
+			return null;		
+		}
 	}
+	
+	
+	// fetch list of user specific text from db
+List<TextModel> getUserTexts(String userName){		
+	
+	try{
+	List<TextModel> listOfAllUserTexts = textRepository.findByUserName(userName);
+	return listOfAllUserTexts;	
+	}
+	
+	catch(Exception ex){
+		
+		logger.debug( " This is an error " + " " + ex.getMessage());
+		
+		return null;
+	}
+	}
+
+// update text entity with user response text
+		
+TextModel  UpdateText(Long id, String userResponse){
+	
+	try{
+	int x = textRepository.updateResponseText (id, userResponse);
+		
+		return textRepository.findByOid(id);
+	
+	}
+		catch(Exception ex){
+			logger.debug(ex.getMessage());
+			return null;	
+		}
+	     
+}
+     	
+// save text in db	
+TextModel saveText(TextModel text) {
+	
+	try {
+	textRepository.save(text);
+	return text;
+	}
+	catch(Exception ex){
+		logger.debug(ex.getMessage());	
+		
+		return null;
+	}
+}
+
+TextModel getTextById(Long texId){
+	
+	try {
+	TextModel txt =  textRepository.findByOid(texId);
+	return txt;
+	}
+	
+	catch(Exception ex ){
+		logger.debug("Getting textById returns :" , ex.getMessage());
+		return null;
+	}
+	
+}
 }
